0a6a61835af43797e51085977357b64f3633c71c
eoinwhitney <whitneyeoin@gmail.com>
2016-05-10 17:33:30 -0700

    added nav controller and second view

---
+++ funfactspractice/funfactspractice/countryViewController.h
@@ -0,0 +1,13 @@
+//
+//  countryViewController.h
+//  funfactspractice
+//
+//  Created by Janet Lustgarten on 5/10/16.
+//  Copyright © 2016 Eoin Whitney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface countryViewController : UIViewController
+
+@end
