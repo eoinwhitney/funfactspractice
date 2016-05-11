6a7835a4c152f7972c1d9928cc533e2d14733d48
eoinwhitney <whitneyeoin@gmail.com>
2016-05-11 12:58:38 -0700

    added another table view and maps to represent the stats

---
+++ funfactspractice/funfactspractice/exploreTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  exploreTableViewController.h
+//  funfactspractice
+//
+//  Created by Janet Lustgarten on 5/11/16.
+//  Copyright © 2016 Eoin Whitney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface exploreTableViewController : UITableViewController
+
+@end
